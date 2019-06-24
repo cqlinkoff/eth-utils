74544b41b77d442703d6a7ad1b054fa3757eb019
kagawagao <kingsongao1221@gmail.com>
2019-06-24 23:21:00 +0800

    fix: fix concatSig

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ const bridge = new WKBridge({
   namespace: 'ethUtils'
 })
 
+export const padWithZeroes = (number, length) => {
+  var myString = '' + number
+  while (myString.length < length) {
+    myString = '0' + myString
+  }
+  return myString
+}
+
+export const concatSig = (v, r, s) => {
+  const rSig = ethUtil.fromSigned(r)
+  const sSig = ethUtil.fromSigned(s)
+  const vSig = ethUtil.bufferToInt(v)
+  const rStr = padWithZeroes(ethUtil.toUnsigned(rSig).toString('hex'), 64)
+  const sStr = padWithZeroes(ethUtil.toUnsigned(sSig).toString('hex'), 64)
+  const vStr = ethUtil.stripHexPrefix(ethUtil.intToHex(vSig))
+  return ethUtil.addHexPrefix(rStr.concat(sStr, vStr)).toString('hex')
+}
+
 export const recoverPublicKey = (hash, sig) => {
   const signature = ethUtil.toBuffer(sig)
   const sigParams = ethUtil.fromRpcSig(signature)
@@ -21,7 +39,7 @@ export const personalSign = (privateKey, msg) => {
   const message = ethUtil.toBuffer(msg)
   const msgHash = ethUtil.hashPersonalMessage(message)
   const sig = ethUtil.ecsign(msgHash, privateKey)
-  const serialized = ethUtil.bufferToHex(this.concatSig(sig.v, sig.r, sig.s))
+  const serialized = ethUtil.bufferToHex(concatSig(sig.v, sig.r, sig.s))
   bridge.postMessage('setPersonalSignResult', serialized)
   return serialized
 }
